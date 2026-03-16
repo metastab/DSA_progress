9c0b2470b8fcf4d2260690fb9e408456304e091f
RayMiller-ui <miller03ray@gmail.com>
2026-03-16 18:24:34 +0530

    vector

---
+++ vector.cpp
@@ -0,0 +1,14 @@
+//syntax, boilerplate and stuff
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+
+    vector <int> Luther = { 1, 2, 3 };
+    for (int x : Luther) {
+        cout << x << " ";
+    }
+    return 0;
+}
